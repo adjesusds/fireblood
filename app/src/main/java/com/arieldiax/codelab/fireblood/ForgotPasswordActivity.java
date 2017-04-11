a0cdd28baeb490061ba181eb51c3c6631e3b1900
Ariel de Jesús Díaz Sánchez <arieldiax@gmail.com>
2017-04-10 22:12:38 -0400

    Close issue #12
    Description:
    * Adds the initial layout for the forgot password activity.

--- app/src/main/java/com/arieldiax/codelab/fireblood/ForgotPasswordActivity.java
+++ app/src/main/java/com/arieldiax/codelab/fireblood/ForgotPasswordActivity.java
@@ -5,17 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class SignInActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     /**
-     * Text view field for forgot your password.
+     * Text view field for sign in.
      */
-    private TextView mForgotYourPasswordTextView;
+    private TextView mSignInTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_forgot_password);
         initUi();
         initListeners();
     }
@@ -24,18 +24,18 @@ public class SignInActivity extends AppCompatActivity {
      * Initializes the user interface view bindings.
      */
     private void initUi() {
-        mForgotYourPasswordTextView = (TextView) findViewById(R.id.forgot_your_password_text_view);
+        mSignInTextView = (TextView) findViewById(R.id.sign_in_text_view);
     }
 
     /**
      * Initializes the event listener view bindings.
      */
     private void initListeners() {
-        mForgotYourPasswordTextView.setOnClickListener(new View.OnClickListener() {
+        mSignInTextView.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                ViewUtils.startCustomActivity(SignInActivity.this, ForgotPasswordActivity.class, false);
+                finish();
             }
         });
     }

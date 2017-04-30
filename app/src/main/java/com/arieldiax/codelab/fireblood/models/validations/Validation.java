783c9a2d62807e258daee86bef5638806fa6b76f
Ariel de Jesús Díaz Sánchez <arieldiax@gmail.com>
2017-04-30 13:26:24 -0400

    Close issue #19

--- app/src/main/java/com/arieldiax/codelab/fireblood/models/validations/Validation.java
+++ app/src/main/java/com/arieldiax/codelab/fireblood/models/validations/Validation.java
@@ -16,6 +16,7 @@ public class Validation {
     public static final String REGEX_NOT_EMPTY = "^.+$";
     public static final String REGEX_EMAIL = "^[\\w!#$%&'*+\\/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+\\/=?`{|}~^-]+)*@(?:[A-Za-z0-9-]+\\.)+[A-Za-z]{2,6}$";
     public static final String REGEX_USERNAME = "^[\\w!#$%&'*+\\/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+\\/=?`{|}~^-]+)*$";
+    public static final String REGEX_EMAIL_OR_USERNAME = "^[\\w!#$%&'*+\\/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+\\/=?`{|}~^-]+)*(?:@(?:[A-Za-z0-9-]+\\.)+[A-Za-z]{2,6})?$";
     public static final String REGEX_PASSWORD = "^(?=.{8,32}$)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$";
     public static final String REGEX_PHONE = "^8[024]9[0-9]{7}$";
 
